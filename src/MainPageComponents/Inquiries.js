6b0875c970e42fa495dfe4c95a957a99d1be765a
gurbur <jihwan7577@gmail.com>
2024-06-05 19:46:32 +0900

    Feat: Inquries

--- src/MainPageComponents/Inquiries.js
+++ src/MainPageComponents/Inquiries.js
@@ -1,7 +1,45 @@
 import axios from "axios";
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 0px;
+`;
+
+const Header = styled.h1`
+  font-size: 40px;
+  color: #333;
+  margin-bottom: 20px;
+`;
+
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  margin: 20px 0;
+`;
+
+const Th = styled.th`
+  border-top: 1px solid #000;
+  padding: 20px;
+  text-align: center;
+  font-size: 20px;
+`;
+
+const Td = styled.td`
+  border-top: 1px solid #000;
+  padding: 20px;
+  text-align: center;
+  font-size: 20px;
+`;
 
 function Inquiries() {
+  const inquiries = [
+    {PropertyName: 'Property_38', Media: '직방', InquiryCount: 32},
+    {PropertyName: 'Property_36', Media: '네이버', InquiryCount: 131},
+    {PropertyName: 'Property_36', Media: '네이버', InquiryCount: 131},
+    {PropertyName: 'Property_36', Media: '네이버', InquiryCount: 131},
+    {PropertyName: 'Property_36', Media: '네이버', InquiryCount: 131},
+  ];
 
   const callApi = async()=>{
     try {
@@ -15,8 +53,32 @@ function Inquiries() {
   useEffect(()=>{
       callApi();
     }, []);
-
-  return <div>Inquiries Page</div>;
+  var index = 1;
+  return (
+    <Container>
+      <Header>광고 매체별 문의</Header>
+      <Table>
+        <thead>
+          <tr>
+            <Th>순위</Th>
+            <Th>매물명</Th>
+            <Th>광고 매체 명</Th>
+            <Th>문의 횟수</Th>
+          </tr>
+        </thead>
+        <tbody>
+          {inquiries.map(inquiries => (
+            <tr key={inquiries.cusID}>
+              <Td>{index++}</Td>
+              <Td>{inquiries.PropertyName}</Td>
+              <Td>{inquiries.Media}</Td>
+              <Td>{inquiries.InquiryCount}</Td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
 
 export default Inquiries;
