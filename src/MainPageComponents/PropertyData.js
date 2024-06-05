504c6f1d83344922ed80a060d530ee8d83bacbf3
gurbur <jihwan7577@gmail.com>
2024-06-05 19:39:48 +0900

    Feat: PropertyData

--- src/MainPageComponents/PropertyData.js
+++ src/MainPageComponents/PropertyData.js
@@ -1,7 +1,45 @@
 import axios from "axios";
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 0px;
+`;
+
+const Header = styled.h1`
+  font-size: 40px;
+  color: #333;
+  margin-bottom: 20px;
+`;
+
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  margin: 20px 0;
+`;
+
+const Th = styled.th`
+  border-top: 1px solid #000;
+  padding: 20px;
+  text-align: center;
+  font-size: 20px;
+`;
+
+const Td = styled.td`
+  border-top: 1px solid #000;
+  padding: 20px;
+  text-align: center;
+  font-size: 20px;
+`;
 
 function PropertyData() {
+
+  const property = [
+    {Address: "서울시 관악구 테헤란로 35-75", Grade: "C", LandlordContact:"01000650217", PriceRange: 33433, PropertyID: 1, PropertyName: "Property_1"},
+    {Address: "서울시 관악구 테헤란로 35-75", Grade: "C", LandlordContact:"01000650217", PriceRange: 33433, PropertyID: 1, PropertyName: "Property_1"},
+    {Address: "서울시 관악구 테헤란로 35-75", Grade: "C", LandlordContact:"01000650217", PriceRange: 33433, PropertyID: 1, PropertyName: "Property_1"},
+    {Address: "서울시 관악구 테헤란로 35-75", Grade: "C", LandlordContact:"01000650217", PriceRange: 33433, PropertyID: 1, PropertyName: "Property_1"},
+  ];
 
   const callApi = async()=>{
     try {
@@ -16,7 +54,35 @@ function PropertyData() {
       callApi();
     }, []);
 
-  return <div>Property Data Page</div>;
+  return (
+    <Container>
+      <Header>전체 매물 데이터</Header>
+      <Table>
+        <thead>
+          <tr>
+            <Th>번호</Th>
+            <Th>매물명</Th>
+            <Th>가격대</Th>
+            <Th>주인 연락처</Th>
+            <Th>등급</Th>
+            <Th>주소</Th>
+          </tr>
+        </thead>
+        <tbody>
+          {property.map(property => (
+            <tr key={property.cusID}>
+              <Td>{property.PropertyID}</Td>
+              <Td>{property.PropertyName}</Td>
+              <Td>{property.PriceRange}</Td>
+              <Td>{property.LandlordContact}</Td>
+              <Td>{property.Address}</Td>
+              <Td>{property.Grade}</Td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
 
 export default PropertyData;
